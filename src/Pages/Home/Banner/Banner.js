6894cc5c5ceecd2a09a4551b4731ce8e42dcbfd4
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-11-02 11:09:42 +0600

    about added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,6 @@ import img3 from '../../../assets/images/banner/3.jpg'
 import img4 from '../../../assets/images/banner/4.jpg'
 import img5 from '../../../assets/images/banner/5.jpg'
 import img6 from '../../../assets/images/banner/6.jpg'
-import './Banner.css'
 import BannerItem from './BannerItem';
 
 const bannerData = [
@@ -50,21 +49,13 @@ const bannerData = [
 const Banner = () => {
     return (
         <div>
-            <div className="carousel w-full">
+            <div className="carousel w-full py-10">
                 {
                     bannerData.map(slide => <BannerItem
                         key={slide.id}
                         slide={slide}
                     ></BannerItem>)
                 }
-                {/* <BannerItem image={img1}></BannerItem> */}
-                {/* <div id="slide2" className="carousel-item relative w-full">
-                    <img src={img2} alt='' className="w-full" />
-                    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                        <a href="#slide1" className="hover:bg-pink-600 btn btn-circle mr-5">❮</a>
-                        <a href="#slide3" className="hover:bg-pink-600 btn btn-circle">❯</a>
-                    </div>
-                </div> */}
             </div>
         </div>
     );
