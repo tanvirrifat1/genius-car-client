446d02176f6f6162fd7a398cdfc253998f9b7d0f
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-11-02 14:11:03 +0600

    added firebase

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -6,15 +6,15 @@ import parts from '../../../assets/images/about_us/parts.jpg'
 const About = () => {
     return (
 
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero my-20">
             <div className="hero-content flex-col lg:flex-row">
                 <div className='w-1/2 relative'>
                     <img src={person} alt='' className="w-4/5 h-full rounded-lg shadow-2xl" />
                     <img src={parts} alt='' className="absolute right-5 top-1/2 w-3/5 rounded-lg border-8 shadow-2xl" />
                 </div>
                 <div className='w-1/2'>
-                    <p className="text-2xl font-bold text-orange-600">About Us</p>
-                    <h1 className="text-5xl font-bold">
+                    <p className=" text-2xl font-bold text-orange-600">About Us</p>
+                    <h1 className="my-5 text-5xl font-bold">
                         We are qualified <br />
                         & of experience <br />
                         in this field</h1>
